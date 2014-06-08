b81a32abe095023dc3c2e2678ca45a643e329713
Jim van Mechelen <VMJim@users.noreply.github.com>
2014-06-08 16:59:44 +0200

    Create plotAll.R

---
+++ plotAll.R
@@ -0,0 +1,10 @@
+#plot all: plots 'n' saves ev'rythin' that was asked
+
+# --- plot 1 ---
+source("plot1.R")
+# --- plot 2 ---
+source("plot2.R")
+# --- plot 3 ---
+source("plot3.R")
+# --- plot 4 ---
+source("plot4.R")
